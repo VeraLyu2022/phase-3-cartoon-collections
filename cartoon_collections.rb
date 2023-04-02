689053b9797c07ebc56b9f7f4a02d8dbde23bc11
Vera Lyu <465784942@qq.com>
2023-04-02 21:35:26 +1000

    i am tired, it is late

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,36 @@
-def roll_call_dwarves# code an argument here
+require 'pry'
+def roll_call_dwarves(array_dwarfs)# code an argument here
   # Your code here
+  array_dwarfs.each_with_index do |name, index|
+    puts "#{index+1}. #{name}"
+  end
+
 end
 
-def summon_captain_planet# code an argument here
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
+  modified_planeteer_calls = planeteer_calls.map do |planet|
+    "#{planet.capitalize}!"
+  end
+  modified_planeteer_calls
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls)# code an argument here
   # Your code here
+   calls.any? do |call|
+    character_number = "#{call}".length
+    if character_number > 4
+      true
+    else  false
+    end
+  end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(foods)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-end
+  foods.find do |food|
+   cheese_types.include?(food)
+  end
+end 
